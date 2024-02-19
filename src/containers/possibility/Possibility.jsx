b852a4ba75af3a90397f172004117b0aaea15f55
Aadil0904 <aadilu@outlook.com>
2024-02-19 21:09:21 +0530

    whatgpt3 & features

--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 import "./possibility.css";
+import ai from "../../assets/ai.png";
+// import Possibility from "./possibility.png";
+
 const Possibility = () => {
-  return <div>Possibility</div>;
+  return (
+    <div className="gpt3__possibility section__padding" id="possibility">
+      <div className="gpt3__possibility-image">
+        <img src
+      </div>
+      <div className="gpt3__possibility-container"></div>
+    </div>
+  );
 };
 
 export default Possibility;
